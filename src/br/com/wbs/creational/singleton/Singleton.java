bbe5373de2c81b7d3145342dc5fef5c832b4e5b1
WELLINGTON DE BARROS SANTOS <wbsantos@sompo.com.br>
2021-06-04 12:35:26 -0300

    criação do padrão singleton

---
+++ src/br/com/wbs/creational/singleton/Singleton.java
@@ -0,0 +1,51 @@
+package br.com.wbs.creational.singleton;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Regra de negócio: Precisa ter a data e hora em que o objeto foi criado. 
+ * Não poderá deixar qualquer outra classe atualizar a data e hora.
+ * 
+ * @author Wellington de Barros Santos
+ *
+ */
+public class Singleton {
+	
+	private static Singleton thisSingleton;
+	private LocalDateTime objectCreatedAt;
+	
+	
+	private Singleton() {
+		setObjectCreatedAt(LocalDateTime.now());
+	}
+	
+	/*
+	 * De acordo com a regra de negócio o "Set" entra em modo privado 
+	 * para não deixar inserir um valor no atributo data e hora de criação do objeto.
+	 */
+	private void setObjectCreatedAt(LocalDateTime objectCreatedAt) {
+		this.objectCreatedAt = objectCreatedAt;
+	}
+	
+	public LocalDateTime getObjectCreatedAt() {
+		return objectCreatedAt;
+	}
+	
+	/**
+	 * Cria uma instância única da classe Singleton.
+	 * @return O objeto criado do singleton.
+	 */
+	public static Singleton getInstance() {
+		// Verifica se o atributo "thisSingleton" ainda não possui nenhuma instância.
+		if (Objects.isNull(thisSingleton)) {
+			
+			// inicia o atributo "thisSingleton"
+			thisSingleton = new Singleton();
+		}
+		
+		return thisSingleton;
+	}
+	
+
+}
