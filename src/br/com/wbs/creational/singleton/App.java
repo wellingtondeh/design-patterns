ca2cda2aae751846759b38c23597db25897e51c8
WELLINGTON DE BARROS SANTOS <wbsantos@sompo.com.br>
2021-06-05 17:17:23 -0300

    Melhoria no singleton.

--- src/br/com/wbs/creational/singleton/App.java
+++ src/br/com/wbs/creational/singleton/App.java
@@ -3,14 +3,14 @@ package br.com.wbs.creational.singleton;
 public class App {
 	
 	public static void main(String[] args) throws InterruptedException {
-		Singleton singleton = Singleton.getInstance();
 		
 		while (true) {
-			waitAndPrintObjectDateTime(singleton);
+			waitAndPrintObjectDateTime();
 		}
 	}
 	
-	private static void waitAndPrintObjectDateTime(Singleton singleton) throws InterruptedException {
+	private static void waitAndPrintObjectDateTime() throws InterruptedException {
+		Singleton singleton = Singleton.getInstance();
 		System.out.println(singleton.getObjectCreatedAt());
 		Thread.sleep(5000);
 	}
